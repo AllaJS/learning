444613b4dc3cb61f250e96eb91e3f7161a4bf27d
Alla <allochkasemak@gmail.com>
2021-05-26 18:16:44 +0300

    third commit

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
-console.log("Yes");
-console.log("Yes !!!");
+document.addEventListener("DOMContentLoaded", () => {
+  const h1 = document.querySelector('h1');
+  h1.addEventListener('click', () => {
+    alert('Yes, it work');
+  })
+});
