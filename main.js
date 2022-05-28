b66283a7002577fcc0798fb4011b5925765020d0
AllaJS <allochkasemak@gmail.com>
2022-05-28 07:39:39 +0300

    vite init

---
+++ main.js
@@ -0,0 +1,15 @@
+import './style.css';
+import {showAlert} from './js/base';
+
+document.querySelector('#app').innerHTML = `
+  <h1 class='title'>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
+
+const showAlertBtn = document.getElementById('show-alert');
+
+showAlertBtn.addEventListener('click', () => {
+  console.log('clicked');
+  showAlert(showAlertBtn.textContent);
+})
+
