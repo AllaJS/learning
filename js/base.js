3146d2545b626c80cb4684cb69d874b04ed14ad8
AllaJS <allochkasemak@gmail.com>
2022-06-29 19:20:19 +0300

    add images + styles fo header and herobanner

--- js/base.js
+++ js/base.js
@@ -1,3 +1,23 @@
 export const showAlert = (text) => {
   alert(text);
 }
+
+// document.querySelector('#app').innerHTML = `
+//   <h1 class='title'>Hello Vite!</h1>
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+// `
+
+const showAlertBtn = document.getElementById('show-alert');
+const titleConstName = document.getElementById('title');
+const navBar = document.getElementById('navBar');
+
+showAlertBtn?.addEventListener('click', () => {
+  titleConstName.style.color = 'red';
+
+  const isOpen = getComputedStyle(navBar).display;
+  navBar.style.display = isOpen === 'block' ? 'none' : 'block';
+})
+
+
+
+console.log('Alla');
--- main.js
+++ main.js
@@ -1,15 +1,12 @@
-import './style.css';
-// import {showAlert} from './js/base';
+//styles
+import './styles/main.scss';
+import './styles/header.scss';
+import './styles/herobanner.scss';
+import './styles/footer.scss';
 
-// document.querySelector('#app').innerHTML = `
-//   <h1 class='title'>Hello Vite!</h1>
-//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-// `
+//js
+import {showAlert} from './js/base';
 
-// const showAlertBtn = document.getElementById('show-alert');
 
-// showAlertBtn.addEventListener('click', () => {
-//   console.log('clicked');
-//   showAlert(showAlertBtn.textContent);
-// })
+
 
