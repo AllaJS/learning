004e195478d23f86deda72e9931059c7b879305f
AllaJS <allochkasemak@gmail.com>
2022-07-01 12:06:36 +0300

    blocks

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 import './styles/main.scss';
 import './styles/header.scss';
 import './styles/herobanner.scss';
+import './styles/textgroup.scss';
+import './styles/dashboard.scss';
+import './styles/rewievs.scss';
+import './styles/questions.scss';
 import './styles/footer.scss';
 
 //js
